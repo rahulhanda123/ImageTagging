c92b05a8bb28f0b5539d80f8d1a40e7587c40743
rahulhanda123 <rahulhanda123@gmail.com>
2017-04-02 20:30:50 -0400

    added check for empty query

--- src/main/java/final_tagger/SampleServer.java
+++ src/main/java/final_tagger/SampleServer.java
@@ -1,4 +1,5 @@
 package final_tagger;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
@@ -10,49 +11,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+
 @WebServlet("/login")
 public class SampleServer extends HttpServlet {
- 
-    /**
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	HTMLParser parser = new HTMLParser();
 	ImaggaTagging tagger = new ImaggaTagging();
 	fileDownloader downloader = new fileDownloader();
-	
+
 	protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-         
-        // read form fields
-        String searchQuery = request.getParameter("imagequery");
-         
-        //get first image url
-        Map<String, String> url = parser.getImageValues(searchQuery);
-        String urlName = url.get("URL");
-        
-        
-		String jsonObject="";
-		try {
-			jsonObject = tagger.getImageTags(url.get("URL"));
-			downloader.downloadImage(searchQuery, urlName, jsonObject);				
-		} catch (UnirestException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			HttpServletResponse response) throws ServletException, IOException {
+
+		// read form fields
+		String searchQuery = request.getParameter("imagequery");
+		if (searchQuery.isEmpty()) {
+			PrintWriter writer = response.getWriter();
+			String htmlRespone = "<html>";
+			htmlRespone += "<h2>" + "The search query cannot be empty."
+					+ "</h2><br/>";
+			htmlRespone += "</html>";
+			// return response
+			writer.println(htmlRespone);
+		} else {
+			// get first image url
+			Map<String, String> url = parser.getImageValues(searchQuery);
+			String urlName = url.get("URL");
+
+			String jsonObject = "";
+			try {
+				jsonObject = tagger.getImageTags(url.get("URL"));
+				downloader.downloadImage(searchQuery, urlName, jsonObject);
+			} catch (UnirestException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			// get response writer
+			PrintWriter writer = response.getWriter();
+			String htmlRespone = "<html>";
+			htmlRespone += "<h2>"
+					+ "The first google image for given query is :"
+					+ "</h2><br/>";
+			htmlRespone += "<img src='" + "/final_tagger/image?imagequery="
+					+ searchQuery + "' height=\"300\" width=\"300\"/><br/>";
+			htmlRespone += "<div> <h3>The Tags for the above image are :</h3></br><h4>"
+					+ jsonObject + "</h4></div>";
+			htmlRespone += "</html>";
+
+			// return response
+			writer.println(htmlRespone);
 		}
-		
-		
-        // get response writer
-        PrintWriter writer = response.getWriter();
-        String htmlRespone = "<html>";
-        htmlRespone += "<h2>" + "The first google image for given query is :" + "</h2><br/>";      
-        htmlRespone += "<img src='" + "/final_tagger/image?imagequery="+searchQuery+ "' height=\"300\" width=\"300\"/><br/>";
-        htmlRespone += "<div> <h3>The Tags for the above image are :</h3></br><h4>"+jsonObject+"</h4></div>";
-        htmlRespone += "</html>";
-         
-        // return response
-        writer.println(htmlRespone);
-         
-    }
- 
+	}
+
 }
